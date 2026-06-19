24ff8276e0cee212e753403e1349c30471a98001
Jorvict <jorvict15@gmail.com>
2026-06-18 22:00:48 -0500

    Se cambio nombre proyeectos arreglos, whily y For

--- For/Program.cs
+++ For/Program.cs
@@ -10,6 +10,9 @@
 };
 
 // El For puede tener varias condicionales siempre que se concatenen con ampersan &&
+// For tiene 3 secciones, la primera es para declarar y definir variables/elementos
+// La segunda sección es para indicar la condicional que se debe cumplir para que siga iterando
+// La tercera sección es un fragmento de codigo que se ejecutara al finalizar la iteración, se usa para incrementar contador
 bool run = true;
 for(int i = 0; i < friends.Length && run; i++)
 {
