b2010704aab210beef757ffa8f87872dbbd1145b
Jorvict <jorvict15@gmail.com>
2026-07-30 23:49:55 -0500

    Se renombraron proyectos dentro de la solución y se agrego modulo 12 sobrecarga

--- Sobrecarga/Program.cs
+++ Sobrecarga/Program.cs
@@ -16,7 +16,7 @@ class Math
     // Podemos hacer una sobrecarga de función, lo que consiste en tener 2 o mas funciones con el mismo nombre
     // Pero que ejecuten cosas distintas EN BASE A LOS TIPOS Y CANTIDAD DE DATOS QUE RECIBIMOS, no se va a guiar
     // de lo que retornamos, sino de lo que le pasamos, por ejemplo en la primera función pasamos ints y en la
-    // segunda función pasamos strings   pesar de que ambos retornan ints, lo que no podriamos es por ejemplo
+    // segunda función pasamos strings, a pesar de que ambos retornan ints, lo que no podriamos es por ejemplo
     // poner una tercera función que retorne string pero que reciba los mismos ints que la segunda función ya que ahí
     // no podría diferenciar la sobrecarga porque los parametros y sus tipos de dato serían iguales
     public int Sum(string a, string b)
